8b402ff7bec6ba86fe4d31bd403cef2998ca91df
Stalkermtx <106044847+Stalkermtx@users.noreply.github.com>
2024-02-10 22:14:46 -0400

    Update config.js

--- mult/config.js
+++ mult/config.js
@@ -1,8 +1,8 @@
 config = {
-  dono:"559184035474", //seu número com 55+ddd sem o 9
+  dono:"5598143061", //seu número com 55+ddd sem o 9
   tempo_teste:"3", //tempo do teste em horas
-  valorLogin: 0.10, //valor do login SSH
-  nomeLoja:"Loja teste", //nome da sua loja virtual
+  valorLogin: 15.00, //valor do login SSH
+  nomeLoja:"Start Net", //nome da sua loja virtual
   linkApp:"LinkAqui", //Link do seu app, playStore/Mediafire
   token_mp:"TokenAqui" //seu token do mercado pago
 }
